aafe0494034ebcaea7bbd46c52e55e5072b6b9a8
jsmolka <jsmolka@pm.me>
2018-04-02 01:19:57 +0200

    add dominant color algorithm

--- src/utils/colorutil.cpp
+++ src/utils/colorutil.cpp
@@ -2,11 +2,11 @@
 
 QColor ColorUtil::darken(QColor color, qreal factor)
 {
-    qreal red = (qreal) color.red() * factor;
-    qreal green = (qreal) color.green() * factor;
-    qreal blue = (qreal) color.blue() * factor;
+    qreal r = (qreal) color.red() * (1 - factor);
+    qreal g = (qreal) color.green() * (1 - factor);
+    qreal b = (qreal) color.blue() * (1 - factor);
 
-    return QColor(red, green, blue);
+    return QColor(r, g, b);
 }
 
 QColor ColorUtil::averageColor(QImage image)
@@ -21,6 +21,7 @@ QColor ColorUtil::averageColor(QImage image)
     for (quint32 i = 0; i < pixelCount; i++)
     {
         QColor pixel = QColor(pixels[i]);
+
         red += pixel.red();
         green += pixel.green();
         blue += pixel.blue();
@@ -33,6 +34,58 @@ QColor ColorUtil::averageColor(QImage image)
     return QColor(red, green, blue);
 }
 
+QColor ColorUtil::dominantColor(QImage image)
+{
+    const quint32 RANGE = 360;
+
+    QRgb *pixels = (QRgb *) image.bits();
+    quint32 pixelCount = image.height() * image.width();
+
+    std::array<quint32, RANGE> hues;
+    std::array<quint32, RANGE> saturations;
+    std::array<quint32, RANGE> values;
+
+    hues.fill(0);
+    saturations.fill(0);
+    values.fill(0);
+
+    for (quint32 i = 0; i < pixelCount; i++)
+    {
+        QColor pixel = QColor(pixels[i]);
+        QColor hsv = pixel.toHsv();
+
+        qint32 hue = hsv.hsvHue();
+        quint32 saturation = hsv.hsvSaturation();
+        quint32 value = hsv.value();
+
+        if (hue == -1)
+            hue = 0;
+
+        hues[hue]++;
+        saturations[hue] += saturation;
+        values[hue] += value;
+    }
+
+    quint32 hue;
+    quint32 satuation;
+    quint32 value;
+
+    quint32 hueCount = hues[0];
+    for (quint32 i = 1; i < RANGE; i++)
+    {
+        if (hues[i] > hueCount)
+        {
+            hueCount = hues[i];
+            hue = i;
+        }
+    }
+
+    satuation = saturations[hue] / hueCount;
+    value = values[hue] / hueCount;
+
+    return QColor::fromHsv(hue, satuation, value);
+}
+
 QColor ColorUtil::backgroundColor(QPixmap image)
 {
     return backgroundColor(image.toImage());
@@ -40,5 +93,5 @@ QColor ColorUtil::backgroundColor(QPixmap image)
 
 QColor ColorUtil::backgroundColor(QImage image)
 {
-    return ColorUtil::darken(averageColor(image));
+    return darken(averageColor(image), 0.6);
 }
--- src/utils/colorutil.hpp
+++ src/utils/colorutil.hpp
@@ -1,14 +1,14 @@
 #ifndef COLORUTIL_HPP
 #define COLORUTIL_HPP
 
-#include <QImage>
 #include <QPixmap>
 
 class ColorUtil
 {
 public:
-    static QColor darken(QColor color, qreal factor = 0.6);
+    static QColor darken(QColor color, qreal factor);
     static QColor averageColor(QImage image);
+    static QColor dominantColor(QImage image);
     static QColor backgroundColor(QPixmap image);
     static QColor backgroundColor(QImage image);
 };
